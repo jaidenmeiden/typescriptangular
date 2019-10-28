73ea51041bb3fa41bf51f94dd83322ed93c88b5b
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-28 11:33:53 -0500

    Clases

---
+++ Transporte.ts
@@ -0,0 +1,12 @@
+class Transporte {
+    private velocidad: number;
+    private formaDesplazamiento: number;
+
+    constructor(
+        velovidad: number,
+        formaDesplazamiento: number
+    ) {
+        this.velocidad = velovidad;
+        this.formaDesplazamiento = formaDesplazamiento;
+    }
+}
